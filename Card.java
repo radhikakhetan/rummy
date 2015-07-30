579a8d831de2f43dd7733d900de01b3bc34ee722
Aravind Srivatsan <hari.srivats@gmail.com>
2015-07-30 18:12:32 +0530

    Override equals method to compare cards and added other methods

--- Card.java
+++ Card.java
@@ -1,24 +1,36 @@
-
-
 public class Card {
-	int rank ;
-	Card ( int i ) {
-		rank = i ;
+	int rank;
+	final int SET = 13;
+
+	Card(int i) {
+		rank = i;
 	}
-	boolean isEqual(Card c) {
-		return c.rank==this.rank ;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+
+		if (!(o instanceof Card))
+			return false;
+
+		return ((Card) o).rank == this.rank;
 	}
+
 	boolean isPrevious(Card c) {
-		return this.rank + 1 == c.rank ;
+		return this.rank + 1 == c.rank;
 	}
+
 	boolean isNext(Card c) {
-		return this.rank == c.rank + 1 ; 
+		return this.rank == c.rank + 1;
 	}
+
 	Card findNext() {
-		return new Card(this.rank + 1 ) ;
+		return new Card(this.rank + 1);
 	}
-	Card findPrevioud() {
-		return new Card(this.rank - 1) ;
+
+	Card findPrevious() {
+		return new Card(this.rank - 1);
 	}
-	
+
 }
