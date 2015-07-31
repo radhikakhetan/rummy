fc20e0fc544abe0e8d9ae3ac9dccdbbdd0dd014c
Bhavana Jain <jain.bhavana1@gmail.com>
2015-07-31 10:06:09 +0530

    triplet added

--- Hand.java
+++ Hand.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Hand {
 	ArrayList<Card> cards;
 	boolean canasta;
 	int requiredCards;
 	int jokerCount;
-	
+
 	public Hand() {
 		cards = new ArrayList<Card>();
 		requiredCards = 0;
@@ -16,28 +17,49 @@ public class Hand {
 		cards.add(c);
 	}
 
-	private ArrayList<ArrayList<Card>> findSequences() {
-		ArrayList<ArrayList<Card>> sequence = new ArrayList<ArrayList<Card>>();
+	private List<ArrayList<Card>> findSequences() {
+		List<ArrayList<Card>> sequence = new ArrayList<ArrayList<Card>>();
 		int count = 0;
 		sequence.get(count).add(cards.get(0));
 		for (int i = 1; i < cards.size(); i++) {
-			if (cards.get(i).isNext(cards.get(i-1))) {
+			if (cards.get(i).isNext(cards.get(i - 1))) {
 				sequence.get(count).add(cards.get(i));
-			}
-			else {
+			} else {
 				count++;
 			}
 		}
 		return sequence;
 	}
-	
+
+	private List<ArrayList<Card>> findTriplets() {
+		List<ArrayList<Card>> triplet = new ArrayList<ArrayList<Card>>();
+		int count = 0;
+		triplet.get(count).add(cards.get(0));
+		for (int i = 1; i < cards.size(); i++) {
+			if ((cards.get(i).rank) % 13 == cards.get(i - 1).rank % 13
+					&& cards.get(i).rank != cards.get(i - 1).rank) {
+				triplet.get(count).add(cards.get(i));
+			} else {
+				count++;
+			}
+		}
+		return triplet;
+	}
+
 	private void removeCanasta() {
 	}
-	
+
 	public int cardsToDeclare() {
 		removeCanasta();
 		return requiredCards;
 	}
-	
-	
+	void print(){
+		for(Card c : cards){
+			System.out.println(c.toString());
+		}
+	}
+	public static void main() {
+
+	}
+
 }
